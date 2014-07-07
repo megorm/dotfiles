ecdf7c131ef200e80f2b99d9d82ea045ce1052fd
megorm <megorm.w@gmail.com>
2014-07-07 16:15:57 +0200

    add some aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,3 +6,12 @@ alias tmux="tmux -2"
 alias psa="ps aux"
 alias psg="ps aux | grep "
 alias psr='ps aux | grep ruby'
+
+# human readable units
+alias du='du -h -d 2'
+
+# Common
+alias tf='tail -f'
+alias cl='clear'
+alias mkdir='mkdir -p'
+
